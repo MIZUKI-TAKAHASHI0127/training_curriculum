7fda9076378fc41125c52ffa5ccf78894a7f71f6
MIZUKI-TAKAHASHI0127 <blue.rose.lost.eden@gmail.com>
2023-06-04 20:24:03 +0900

    プルリクエスト作成用

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -43,6 +43,5 @@ class CalendarsController < ApplicationController
     days = { month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans, wday: wdays[wday_num + x]}
       @week_days.push(days)
     end
-
   end
 end
